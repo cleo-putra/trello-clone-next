b4d7bb7a37599e00dd79b50c6b23b5198784ccb2
cleo-putra <abdigoldi@gmail.com>
2023-06-12 15:27:42 +0700

    apps deploy

---
+++ lib/getTodosGroupedByColumn.ts.ts
@@ -0,0 +1,60 @@
+import { databases } from "@/appwrite"
+import { data } from "autoprefixer"
+
+export const getTodosGroupedByColumn = async() => {
+  const data = await databases.listDocuments(
+    process.env.NEXT_PUBLIC_DATABASE_ID!,
+    process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
+  );
+
+  const todos = data.documents;
+
+  const columns = todos.reduce((acc, todo) => {
+    if(!acc.get(todo.status)) {
+      acc.set(todo.status, {
+        id: todo.status,
+        todos: []
+      })
+    }
+
+    acc.get(todo.status)!.todos.push({
+      $id: todo.$id,
+      $createdAt: todo.$createdAt,
+      title: todo.title,
+      status: todo.status,
+      // get image if exists on to do 
+      ...(todo.image && {images: JSON.parse(todo.image)})
+
+    });
+
+    return acc;
+
+  }, new Map<TypedColumn, Column>)
+
+  // if columns doesnt have inprogess, todo, and doesNotMatch. add them with empty todos
+  const columnTypes: TypedColumn[] = ["todo", "inprogress", "done"];
+  for (const columnType of columnTypes) {
+    if (!columns.get(columnType)) {
+      columns.set(columnType, {
+        id: columnType,
+        todos: [],
+      });
+    }
+  }
+
+  console.log(columns);
+
+  // sort columns by columnTypes
+  const sortedColumns = new Map(
+    Array.from(columns.entries()).sort((a, b) => (
+      columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0]) 
+    ))
+  );
+
+  const board: Board = {
+    columns: sortedColumns
+  }
+
+  return board;
+};
+
---
+++ lib/uploadImage.ts
@@ -0,0 +1,16 @@
+import { ID, storage } from "@/appwrite";
+
+
+const uploadImage = async(file: File) => {
+  if (!file)  return;
+
+  const fileUploaded = await storage.createFile(
+    '64747dc400555067cc8e',
+    ID.unique(),
+    file
+  );
+
+  return fileUploaded;
+};
+
+export default uploadImage
